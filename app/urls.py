0df85bf21022ec68e3e696b98bea0e9e8d4d98e5
unknown <juanfgallo78@gmail.com>
2024-04-08 22:55:48 -0300

    commit parcial

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from animals.views import animal_view, animal_create,animal_delete
+from animals.views import animal_view, animal_create,animal_delete, animal_update
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('animal/list/', animal_view),  
     path('animal/save/', animal_create),
-    path('animal/delete/', animal_delete),  
+    path('animal/delete/', animal_delete), 
+    path('animal/update/',animal_update), 
 ]
